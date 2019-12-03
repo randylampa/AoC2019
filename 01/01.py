1e1912c2b3d17c9feab8ec30b009603e27ebfe9b
René Panák <panak@advertising-media.cz>
2019-12-03 10:01:02 +0100

    updated template + refactored 01

---
+++ 01/01.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*-coding-*-: utf-8
+#
+# AoC 2019
+# RandyLampa
+
+
+def get_input_test():
+    masses = [12, 14, 1969, 100756]
+    return masses
+
+
+def get_input():
+    fh = open("input-1", "r")
+    masses = list(map(lambda x: int(x.strip()), fh.readlines()))
+    return masses
+
+
+def mass2fuel(mass):
+    fuel = mass // 3 - 2
+    return fuel
+
+
+def solve1():
+    # test input
+    masses = get_input_test()
+
+    # input
+    masses = get_input()
+
+    print("Input masses:")
+    print(masses)
+
+    fuel = list(map(mass2fuel, masses))
+
+    print("Fuel requirements:")
+    print(fuel)
+
+    total = sum(fuel)
+
+    print("Total fuel requirement:")
+    print(total)
+    return total
+
+
+def solve2():
+    pass
+
+
+if __name__ == "__main__":
+
+    print("="*40)
+    print("Solve 1 status:", solve1())
+    print("="*40)
+
+    print("="*40)
+    print("Solve 2 status:", solve2())
+    print("="*40)
--- aoc19_tpl.py
+++ aoc19_tpl.py
@@ -4,6 +4,21 @@
 # AoC 2019
 # RandyLampa
 
-from sys import *
 
-print('Hello')
+def solve1():
+    pass
+
+
+def solve2():
+    pass
+
+
+if __name__ == "__main__":
+
+    print("="*40)
+    print("Solve 1 status:", solve1())
+    print("="*40)
+
+    print("="*40)
+    print("Solve 2 status:", solve2())
+    print("="*40)
